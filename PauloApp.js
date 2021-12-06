a88132cd12c0cfa298ab78f579d07daf061bfec8
nayelim25 <78129670+nayelim25@users.noreply.github.com>
2021-12-05 22:09:51 -0600

    Add files via upload

---
+++ PauloApp.js
@@ -0,0 +1,40 @@
+var counter = 0;
+const Submit = document.querySelector('#Submit');
+
+const correctAnswers = ['Gunma, Japan', 'Soichiro Honda', 'Rotary Engine','ATTESA', 'Makinen', 'Aristo', 'Wangan Midnight','NewSports','EJ20','Mitsubishi']
+
+Submit.onclick = function()
+{
+for(correctAnswer in correctAnswers)
+    {
+        console.log(`input[name="Q${parseInt(correctAnswer) + 1}"]`)
+        var rbs = document.querySelectorAll(`input[name="Q${parseInt(correctAnswer) + 1}"]`)
+        var selectedValue;
+        for (const rb of rbs){
+            if(rb.checked)
+            {
+                selectedValue = rb.value;
+                if(selectedValue == correctAnswers[correctAnswer])
+                {
+                    counter++;
+                }
+            }
+        }
+    }
+    alert("Your total score is " + counter + " out " + correctAnswers.length + ".");
+    counter = 0;
+
+
+document.querySelector('input[name="Q1"]:checked').checked = false;
+document.querySelector('input[name="Q2"]:checked').checked = false;
+document.querySelector('input[name="Q3"]:checked').checked = false;
+document.querySelector('input[name="Q4"]:checked').checked = false;
+document.querySelector('input[name="Q5"]:checked').checked = false;
+document.querySelector('input[name="Q6"]:checked').checked = false;
+document.querySelector('input[name="Q7"]:checked').checked = false;
+document.querySelector('input[name="Q8"]:checked').checked = false;
+document.querySelector('input[name="Q9"]:checked').checked = false;
+document.querySelector('input[name="Q10"]:checked').checked = false;
+
+
+};
---
+++ functions.js
@@ -0,0 +1,42 @@
+var counter = 0;
+const Submit = document.querySelector('#Submit');
+
+const correctAnswers = ['Meredith Grey', 'Seattle Grace', 'Amelia','Neurology', 'Ellen Pompeo', 'Derek Shepherd', 'Ellis','Bailey','Alex','Pediatrics']
+
+Submit.onclick = function()
+{
+for(correctAnswer in correctAnswers)
+    {
+        console.log(`input[name="Q${parseInt(correctAnswer) + 1}"]`)
+        var rbs = document.querySelectorAll(`input[name="Q${parseInt(correctAnswer) + 1}"]`)
+        var selectedValue;
+        for (const rb of rbs){
+            if(rb.checked)
+            {
+                selectedValue = rb.value;
+                if(selectedValue == correctAnswers[correctAnswer])
+                {
+                    counter++;
+                }
+            }
+        }
+    }
+    alert("Your total score is " + counter + " out " + correctAnswers.length + ".");
+    counter = 0;
+
+
+document.querySelector('input[name="Q1"]:checked').checked = false;
+document.querySelector('input[name="Q2"]:checked').checked = false;
+document.querySelector('input[name="Q3"]:checked').checked = false;
+document.querySelector('input[name="Q4"]:checked').checked = false;
+document.querySelector('input[name="Q5"]:checked').checked = false;
+document.querySelector('input[name="Q6"]:checked').checked = false;
+document.querySelector('input[name="Q7"]:checked').checked = false;
+document.querySelector('input[name="Q8"]:checked').checked = false;
+document.querySelector('input[name="Q9"]:checked').checked = false;
+document.querySelector('input[name="Q10"]:checked').checked = false;
+
+
+};
+
+
